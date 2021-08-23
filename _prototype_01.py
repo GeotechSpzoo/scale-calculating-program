e187bb882217d3ff2fa0d3f47e6cab9e1c3e958a
pawel.drelich <drelich_pawel@o2.pl>
2021-08-23 19:56:00 +0200

    Reformat and parameters change. Finding reference circle work very good.

--- _prototype_01.py
+++ _prototype_01.py
@@ -1,23 +1,23 @@
-import cv2
-
 import functions as f
+from os import walk
 
-# img = f.load_image("sprey1/sprey1_dot100.jpg")
-# img = f.load_image("sprey1/sprey2_dot100.jpg")
-img = f.load_image("str1/0_str1_0_max_Normal.jpg")
-# contrasted = f.contrast_increase_clahe(img)
-# for i in range(1):
-#     contrasted = f.contrast_increase_clahe(contrasted)
-gray = f.bgr_to_gray(img)
-blurred = f.blur_bilateral_filter_min(gray)
-for i in range(10):
+
+def find_ref_object(path_to_image):
+    print(path_to_image)
+    img = f.load_image(path_to_image)
+    gray = f.bgr_to_gray(img)
+    blurred = f.blur_bilateral_filter_min(gray)
+    for i in range(35):
+        blurred = f.blur_bilateral_filter_min(blurred)
     blurred = f.blur_bilateral_filter_min(blurred)
-cv2.waitKey(0)
-# for i in range(255):
-#     binary = f.gray_to_binary(blurred, i)
-# cv2.waitKey(0)
-binary = f.gray_to_binary(blurred, 78)
-edged = f.detect_edges_raw_canny(binary, 25, 100)
-# eroded = f.erode_dilate(edged)
-f.find_contours_and_draw_them(f.gray_to_bgr(gray), edged, "REF OBJECTS FOUND", 69)
-cv2.waitKey(0)
+    binary = f.gray_to_binary(blurred, 77, True)
+    edged = f.detect_edges_raw_canny(binary, 25, 100)
+    # eroded = f.erode_dilate(edged)
+    f.find_contours_and_draw_them(f.gray_to_bgr(gray), edged, path_to_image, 69, 112)
+
+
+pathToPhotos = "testAlgo1/"
+file_names = next(walk(pathToPhotos), (None, None, []))[2]
+for file_name in file_names:
+    find_ref_object(pathToPhotos + file_name)
+# find_ref_object("testAlgo1/0_testAlgo1_0_min_Normal.jpg")
--- functions.py
+++ functions.py
@@ -12,11 +12,13 @@ KEY_BACKSPACE = 8
 # ONE_INCH_IN_MILLIMETERS = 25.4
 # ONE_MILLIMETER_IN_INCHES = 1 / 25.4
 REF_OBJ_SIZE_IN_MILLIMETERS = 0.1  # 0.1 mm = 0.003937 inch
+
+
 # REF_OBJ_SIZE_IN_INCH = ONE_MILLIMETER_IN_INCHES * REF_OBJ_SIZE_IN_MILLIMETERS  # 0.1 mm = 0.003937 inch
-PIXELS_PER_MILLIMETER = None
+# PIXELS_PER_MILLIMETER = None
 
 
-def contrast_increase_clahe(img):
+def contrast_increase_clahe(img, wait=False):
     # -----Converting image to LAB Color model-----------------------------------
     lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
     # cv2.imshow("lab", lab)
@@ -34,47 +36,52 @@ def contrast_increase_clahe(img):
     # cv2.imshow('limg', limg)
     # -----Converting image from LAB Color model to RGB model--------------------
     final = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
-    cv2.imshow("contrast_increase_clahe", final)
-    cv2.waitKey(0)
+    if wait:
+        cv2.imshow("contrast_increase_clahe", final)
+        cv2.waitKey(0)
     # _____END_____#
     return final
 
 
-def sharpen(img):
+def sharpen(img, wait=False):
     # Sharpen image
     kernel = np.array([[-1.1, -1.1, -1.1],
                        [-1.1, 9.8, -1.1],
                        [-1.1, -1.1, -1.1]])
     sharpened = cv2.filter2D(img, -1, kernel)  # Applying the sharpening kernel to the input image.
-    cv2.imshow("sharpen", sharpened)
-    cv2.waitKey(0)
+    if wait:
+        cv2.imshow("sharpen", sharpened)
+        cv2.waitKey(0)
     return sharpened
 
 
-def gray_to_binary(gray, tresh):
+def gray_to_binary(gray, tresh, wait=False):
     ret, thresholded = cv2.threshold(gray, tresh, 255, cv2.THRESH_BINARY)
-    cv2.imshow(f"gray_to_binary tresh: {tresh}", thresholded)
-    cv2.waitKey(0)
+    if wait:
+        cv2.imshow(f"gray_to_binary tresh: {tresh}", thresholded)
+        cv2.waitKey(0)
     return thresholded
 
 
-def blur_gaussian(gray):
+def blur_gaussian(gray, wait=False):
     k_size = (7, 7)
     sigma = 0
     # GaussianBlur(src, k_size, sigmaX, dst=None, sigmaY=None, borderType=None)
     blurred = cv2.GaussianBlur(gray, k_size, sigma)
-    cv2.imshow(f"blur_gaussian k_size: {k_size}, sigma: {sigma}", gray)
-    cv2.waitKey(0)
+    if wait:
+        cv2.imshow(f"blur_gaussian k_size: {k_size}, sigma: {sigma}", gray)
+        cv2.waitKey(0)
     return blurred
 
 
-def blur_bilateral_filter(gray, d, sigma):
+def blur_bilateral_filter(gray, d, sigma, wait=False):
     d = 4
     sigma = 1
     window_name = f"blur_bilateral_filter d: {d}, sigma: {sigma}"
     blurred = cv2.bilateralFilter(gray, d, sigma, sigma)
-    cv2.imshow(window_name, blurred)
-    cv2.waitKey(0)
+    if wait:
+        cv2.imshow(window_name, blurred)
+        cv2.waitKey(0)
     return blurred
 
 
@@ -83,36 +90,39 @@ def blur_bilateral_filter_min(gray, wait=False):
     sigma = 11
     window_name = f"blur_bilateral_filter d: {d}, sigma: {sigma}"
     blurred = cv2.bilateralFilter(gray, d, sigma, sigma)
-    cv2.imshow(window_name, blurred)
     if wait:
+        cv2.imshow(window_name, blurred)
         cv2.waitKey(0)
     return blurred
 
 
-def load_image(photo_path):
+def load_image(photo_path, wait=False):
     loaded = cv2.imread(photo_path, cv2.IMREAD_COLOR)
-    cv2.imshow(f"load_image path:{photo_path}", loaded)
-    cv2.waitKey(0)
+    if wait:
+        cv2.imshow(f"load_image path:{photo_path}", loaded)
+        cv2.waitKey(0)
     return loaded
 
 
-def bgr_to_gray(bgr_img):
+def bgr_to_gray(bgr_img, wait=False):
     gray = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2GRAY)
-    cv2.imshow("bgr_to_gray", gray)
-    cv2.waitKey(0)
+    if wait:
+        cv2.imshow("bgr_to_gray", gray)
+        cv2.waitKey(0)
     return gray
 
 
-def gray_to_bgr(gray):
+def gray_to_bgr(gray, wait=False):
     bgr = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
-    cv2.imshow("gray_to_bgr", bgr)
-    cv2.waitKey(0)
+    if wait:
+        cv2.imshow("gray_to_bgr", bgr)
+        cv2.waitKey(0)
     return bgr
 
 
 # Find edges using canny edge detector
 # noinspection PyTypeChecker
-def detect_edges_sigma_v(gray_img, sigma=0.33, v=202):
+def detect_edges_sigma_v(gray_img, sigma=0.33, v=202, wait=False):
     # compute the median of the single channel pixel intensities
     # v = np.median(grayim)  # 202.0
     # v = np.float64(20)
@@ -120,13 +130,14 @@ def detect_edges_sigma_v(gray_img, sigma=0.33, v=202):
     lower = int(max(0, (1.0 - sigma) * v))
     upper = int(min(255, (1.0 + sigma) * v))
     edged = cv2.Canny(gray_img, lower, upper)
-    cv2.imshow(f"detect_edges_sigma_v sigma={sigma}, v={v}", edged)
-    cv2.waitKey(0)
+    if wait:
+        cv2.imshow(f"detect_edges_sigma_v sigma={sigma}, v={v}", edged)
+        cv2.waitKey(0)
     return edged
 
 
 # Find edges using canny edge detector
-def detect_edges_auto(gray_img, sigma=0.33):
+def detect_edges_auto(gray_img, sigma=0.33, wait=False):
     # compute the median of the single channel pixel intensities
     v = np.median(gray_img)  # 202.0
     # v = np.float64(20)
@@ -134,25 +145,28 @@ def detect_edges_auto(gray_img, sigma=0.33):
     lower = int(max(0, (1.0 - sigma) * v))
     upper = int(min(255, (1.0 + sigma) * v))
     edged = cv2.Canny(gray_img, lower, upper)
-    cv2.imshow(f"detect_edges_auto sigma={sigma}, v={v}, lower={lower}, upper={upper}", edged)
-    cv2.waitKey(0)
+    if wait:
+        cv2.imshow(f"detect_edges_auto sigma={sigma}, v={v}, lower={lower}, upper={upper}", edged)
+        cv2.waitKey(0)
     return edged
 
 
 # Find edges using canny edge detector
-def detect_edges_raw_canny(gray_img, lower, upper):
+def detect_edges_raw_canny(gray_img, lower, upper, wait=False):
     edged = cv2.Canny(gray_img, lower, upper)
-    cv2.imshow(f"detect_edges_raw_canny lower={lower}, upper={upper}", edged)
-    cv2.waitKey(0)
+    if wait:
+        cv2.imshow(f"detect_edges_raw_canny lower={lower}, upper={upper}", edged)
+        cv2.waitKey(0)
     return edged
 
 
-def erode_dilate(thresholded):
+def erode_dilate(thresholded, wait=False):
     kernel = np.ones((1, 1), np.uint8)
     eroded = cv2.erode(thresholded, kernel, iterations=1)
     dilated = cv2.dilate(eroded, kernel, iterations=1)
-    cv2.imshow("erode_dilate", dilated)
-    cv2.waitKey(0)
+    if wait:
+        cv2.imshow("erode_dilate", dilated)
+        cv2.waitKey(0)
     return dilated
 
 
@@ -160,13 +174,29 @@ def midpoint(ptA, ptB):
     return (ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5
 
 
-def find_contours_and_draw_them(img, edged, window_name, min_size):
-    global PIXELS_PER_MILLIMETER
+def box_area(box):
+    box_size = box[1]
+    box_x = box_size[0]
+    box_y = box_size[1]
+    area = box_x * box_y
+    return area
+
+
+def box_size(box):
+    return box[1]
+
+
+def box_center(box):
+    return box[0]
+
+
+def find_contours_and_draw_them(img, edged, window_name, min_size, max_size, show_all=False, wait=False):
+    pixels_per_millimeter = None
     # find contours in the edge map
     # cnts = cv2.findContours(edged, cv2.RETR_EXTERNAL,
     #                         cv2.CHAIN_APPROX_SIMPLE)
     cnts = cv2.findContours(edged, cv2.RETR_LIST,
-                            cv2.CHAIN_APPROX_SIMPLE)
+                            cv2.CHAIN_APPROX_TC89_L1)
     cnts = imutils.grab_contours(cnts)
     # sort the contours from left-to-right and initialize the
     # 'pixels per metric' calibration variable
@@ -175,89 +205,131 @@ def find_contours_and_draw_them(img, edged, window_name, min_size):
     orig = img
     for c in cnts:
         # if the contour is not sufficiently large, ignore it
-        area = cv2.contourArea(c)
-        if min_size * min_size < area:
-            print("area:", area)
-            # compute the rotated bounding box of the contour
-            # orig = cv2.cvtColor(edged, cv2.COLOR_GRAY2BGR)
-            box = cv2.minAreaRect(c)
-            print("minAreaRect:", box)
-            # cv2.waitKey(0)
-            # noinspection PyUnresolvedReferences
-            box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
-            box = np.array(box, dtype="int")
-
-            # order the points in the contour such that they appear
+        box = cv2.minAreaRect(c)
+        print("box:", box)
+        (box_center_x, box_center_y) = box_center(box)
+        (box_size_x, box_size_y) = box_size(box)
+        area = box_area(box)
+        print("area:", area)
+        size_for_scale_calculation = np.minimum(box_size_x, box_size_y)
+        print("size_for_scale_calculation:", size_for_scale_calculation)
+        # noinspection PyUnresolvedReferences
+        box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
+        box = np.array(box, dtype="int")
+        print("box vertexes:", box)
+        is_appropriate_size = min_size < box_size_x < max_size and min_size < box_size_y < max_size
+        if pixels_per_millimeter is None:
+            if is_appropriate_size:
+                # PIXELS_PER_METRIC = dB / REF_OBJ_SIZE_IN_INCH
+                pixels_per_millimeter = size_for_scale_calculation / REF_OBJ_SIZE_IN_MILLIMETERS
+                # print(f"REF_OBJ_CONTOUR = {c}")
+                print(f"REF_OBJ_BOX = {box}")
+                print(f"PHOTO_SCALE: 1 mm = {'{:.2f}'.format(pixels_per_millimeter)} px")
+        if is_appropriate_size or show_all:
+            # sort the points in the contour such that they appear
             # in top-left, top-right, bottom-right, and bottom-left
             # order, then draw the outline of the rotated bounding
             # box
             box = perspective.order_points(box)
-            cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 1)
-
-            # loop over the original points and draw them
-            for (x, y) in box:
-                cv2.circle(orig, (int(x), int(y)), 3, (0, 0, 255), -1)
-            for [[x, y]] in c:
-                cv2.circle(orig, (int(x), int(y)), 1, (0, 127, 255), -1)
-
-            # unpack the ordered bounding box, then compute the midpoint
-            # between the top-left and top-right coordinates, followed by
-            # the midpoint between bottom-left and bottom-right coordinates
-            (tl, tr, br, bl) = box
-            (tltrX, tltrY) = midpoint(tl, tr)
-            (blbrX, blbrY) = midpoint(bl, br)
-
-            # compute the midpoint between the top-left and top-right points,
-            # followed by the midpoint between the top-righ and bottom-right
-            (tlblX, tlblY) = midpoint(tl, bl)
-            (trbrX, trbrY) = midpoint(tr, br)
-
-            # draw the midpoints on the image
-            # cv2.circle(orig, (int(tltrX), int(tltrY)), 1, (255, 0, 0), -1)
-            # cv2.circle(orig, (int(blbrX), int(blbrY)), 1, (255, 0, 0), -1)
-            # cv2.circle(orig, (int(tlblX), int(tlblY)), 1, (255, 0, 0), -1)
-            # cv2.circle(orig, (int(trbrX), int(trbrY)), 1, (255, 0, 0), -1)
-
-            # draw lines between the midpoints
-            # cv2.line(orig, (int(tltrX), int(tltrY)), (int(blbrX), int(blbrY)),
-            #          (255, 0, 255), 1)
-            # cv2.line(orig, (int(tlblX), int(tlblY)), (int(trbrX), int(trbrY)),
-            #          (255, 0, 255), 1)
-
-            # compute the Euclidean distance between the midpoints
-            dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
-            dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
-            # if the pixels per metric has not been initialized, then
-            # compute it as the ratio of pixels to supplied metric
-            # (in this case, inches)
-            if PIXELS_PER_MILLIMETER is None:
-                # PIXELS_PER_METRIC = dB / REF_OBJ_SIZE_IN_INCH
-                PIXELS_PER_MILLIMETER = dB / REF_OBJ_SIZE_IN_MILLIMETERS
-                # print(f"REF_OBJ_CONTOUR = {c}")
-                print(f"REF_OBJ_BOX = {box}")
-                print(f"PHOTO_SCALE: 1 mm = {'{:.2f}'.format(PIXELS_PER_MILLIMETER)} px")
-            # compute the size of the object
-            dimA = dA / PIXELS_PER_MILLIMETER
-            dimB = dB / PIXELS_PER_MILLIMETER
-            # draw the object sizes on the image
-            # putText(img, text, bottom-left-corner, fontFace, fontScale, color, thickness=None, lineType=None, bottomLeftOrigin=None)
-            cv2.putText(orig,
-                        "{:.2f} mm".format(dimA),
-                        (int(trbrX + 10), int(trbrY)),
-                        cv2.FONT_HERSHEY_SIMPLEX,
-                        0.4,
-                        (0, 255, 127),
-                        1)
-            cv2.putText(orig,
-                        "{:.2f} mm".format(dimB),
-                        (int(tltrX - 15), int(tltrY - 10)),
-                        cv2.FONT_HERSHEY_SIMPLEX,
-                        0.4,
-                        (0, 255, 127),
-                        1)
-            # show the output image
+            draw_contour(c, orig)
+            draw_box(box, orig)
+            draw_corners(box, orig)
+            draw_area(area, box_center_x, box_center_y, orig)
+            if pixels_per_millimeter is None:
+                draw_no_scale(orig, box_center_x, box_center_y)
+            else:
+                draw_dimensions(box, orig, pixels_per_millimeter)
+            if wait:
+                cv2.imshow(f"{window_name} min_size = {min_size}", orig)
+                cv2.waitKey(0)
+            if not show_all:
+                break
     cv2.imshow(f"{window_name} min_size = {min_size}", orig)
     return cv2.waitKey(0)
+
+
+def draw_box(box, orig):
+    # draw box contour
+    cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 1)
+
+
+def draw_contour(c, orig):
+    # draw contour points
+    for [[x, y]] in c:
+        cv2.circle(orig, (int(x), int(y)), 1, (0, 127, 255), -1)
+
+
+def draw_corners(box, orig):
+    # draw box corners/vertexes
+    for (x, y) in box:
+        cv2.circle(orig, (int(x), int(y)), 3, (0, 0, 255), -1)
+
+
+def draw_dimensions(box, orig, pixels_per_millimeter):
+    # unpack the ordered bounding box, then compute the midpoint
+    # between the top-left and top-right coordinates, followed by
+    # the midpoint between bottom-left and bottom-right coordinates
+    (tl, tr, br, bl) = box
+    (tltrX, tltrY) = midpoint(tl, tr)
+    (blbrX, blbrY) = midpoint(bl, br)
+    # compute the midpoint between the top-left and top-right points,
+    # followed by the midpoint between the top-righ and bottom-right
+    (tlblX, tlblY) = midpoint(tl, bl)
+    (trbrX, trbrY) = midpoint(tr, br)
+    # draw lines between the midpoints
+    # cv2.line(orig, (int(tltrX), int(tltrY)), (int(blbrX), int(blbrY)),
+    #          (255, 0, 255), 1)
+    # cv2.line(orig, (int(tlblX), int(tlblY)), (int(trbrX), int(trbrY)),
+    #          (255, 0, 255), 1)
+    # compute the Euclidean distance between the midpoints
+    dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
+    dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
+    # if the pixels per metric has not been initialized, then
+    # compute it as the ratio of pixels to supplied metric
+    # (in this case, inches)
+    draw_object_size(pixels_per_millimeter, dA, dB, orig, tltrX, tltrY, trbrX, trbrY)
+
+
+def draw_no_scale(orig, box_center_x, box_center_y):
+    cv2.putText(orig,
+                "NO SCALE",
+                (int(box_center_x - 20), int(box_center_y - 20)),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.6,
+                (0, 0, 0),
+                2)
+
+
+def draw_object_size(pixels_per_millimeter, dA, dB, orig, tltrX, tltrY, trbrX, trbrY):
+    # compute the size of the object
+    dimA = dA / pixels_per_millimeter
+    dimB = dB / pixels_per_millimeter
+    # draw the object sizes on the image
+    # putText(img, text, bottom-left-corner, fontFace, fontScale, color, thickness=None, lineType=None, bottomLeftOrigin=None)
+    cv2.putText(orig,
+                "{:.2f} mm".format(dimA),
+                (int(trbrX + 10), int(trbrY)),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.4,
+                (0, 255, 127),
+                1)
+    cv2.putText(orig,
+                "{:.2f} mm".format(dimB),
+                (int(tltrX - 15), int(tltrY - 10)),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.4,
+                (0, 255, 127),
+                1)
+
+
+def draw_area(area, box_center_x, box_center_y, orig):
+    cv2.putText(orig,
+                "{:.0f}px2".format(area),
+                (int(box_center_x), int(box_center_y)),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.4,
+                (0, 0, 0),
+                1)
 
 
 def detect_circles(gray, orig):
