770e54c13f82e80eb47ce28cef80d9ee5db56b6c
pawel.drelich <drelich_pawel@o2.pl>
2021-10-15 17:42:09 +0200

    show_info parameter added.

--- exif.py
+++ exif.py
@@ -6,25 +6,27 @@ user_comment = "UserComment"
 image_description = "ImageDescription"
 
 
-def read_tag_value(tag, source_file):
+def read_tag_value(tag, source_file, print_info=False):
     args = ["exiftool", "-s", "-s", "-s", f"-{tag}", f"{source_file}"]
     out, err = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
                                 universal_newlines=True).communicate()
-    print("exif.read_tag_value:", out)
-    # print(f"Exif read: {tag} = {out}")
+    if print_info:
+        print(f"Exif read: {tag} = {out}")
+        print("exif.read_tag_value:", out)
     return out.strip()
 
 
-def write_tag_value(tag, value, source_file):
+def write_tag_value(tag, value, source_file, print_info=False):
     write_args = ["exiftool", "-overwrite_original", f"-{tag}={value}", f"{source_file}"]
     print(f"Exif write: {tag} = {value}")
     out, err = subprocess.Popen(write_args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
                                 universal_newlines=True).communicate()
-    print("exif.write_tag_value:", out)
-    # print(f"Exif write: {tag} = {value}")
+    if print_info:
+        print(f"Exif write: {tag} = {value}")
+        print("exif.write_tag_value:", out)
 
 
-def write_resolution_tags(path_to_file, scale_in_dpmm, original_comment):
+def write_resolution_tags(path_to_file, scale_in_dpmm, original_comment, print_info=False):
     if scale_in_dpmm < 0:
         return
     else:
@@ -41,38 +43,43 @@ def write_resolution_tags(path_to_file, scale_in_dpmm, original_comment):
                f" {path_to_file}"
         out, err = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
                                     universal_newlines=True).communicate()
-        print("exif.write_resolution_tags:", out)
+        if print_info:
+            print("exif.write_resolution_tags:", out)
 
 
-def print_tag_value(tag, source_file):
+def print_tag_value(tag, source_file, print_info=False):
     read_args = ["exiftool", f"-{tag}", f"{source_file}"]
     out, err = subprocess.Popen(read_args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
                                 universal_newlines=True).communicate()
-    print("exif.print_tag_value:", out)
+    if print_info:
+        print("exif.print_tag_value:", out)
 
 
-def print_all_tags(source_file):
+def print_all_tags(source_file, print_info=False):
     args = f"exiftool -a -u -g1 {source_file}"
     out, err = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
                                 universal_newlines=True).communicate()
-    print("exif.print_all_tags:", out)
+    if print_info:
+        print("exif.print_all_tags:", out)
 
 
-def delete_all_tags(source_file):
+def delete_all_tags(source_file, print_info=False):
     args = f"exiftool -all= {source_file}"
     out, err = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
                                 universal_newlines=True).communicate()
-    print("exif.delete_all_tags:", out)
+    if print_info:
+        print("exif.delete_all_tags:", out)
 
 
-def copy_all_tags(source_file, destination_file):
+def copy_all_tags(source_file, destination_file, print_info=False):
     args = f"exiftool -overwrite_original -TagsFromFile {source_file} -all:all>all:all {destination_file}"
     out, err = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
                                 universal_newlines=True).communicate()
-    print("exif.copy_all_tags:", out)
+    if print_info:
+        print("exif.copy_all_tags:", out)
 
 
-def set_new_dpmm(dpmm, source_file):
+def set_new_dpmm(dpmm, source_file, print_info=False):
     dpmm_pattern = r"\d+dpmm"
     new_dpmm = f"{dpmm}dpmm"
     original_tag = read_tag_value(xp_comment, source_file)
@@ -84,7 +91,8 @@ def set_new_dpmm(dpmm, source_file):
         dpmm_replaced = original_tag + new_dpmm + ";"
     else:
         dpmm_replaced = ";" + new_dpmm + ";"
-    print("new_dpmm:", dpmm_replaced)
+    if print_info:
+        print("new_dpmm:", dpmm_replaced)
     write_tag_value(xp_comment, dpmm_replaced, source_file)
 
 
